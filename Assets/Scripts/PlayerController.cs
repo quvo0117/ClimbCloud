c7dd0eca9d86e1a6679ed57a4e8b076325bec6ab
kb7 <kubo20231020@gmail.com>
2024-03-07 11:07:12 +0900

    public flat jumpForce

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
-    float jumpForce = 680.0f;
+    public float jumpForce = 680.0f;  // puclic にすると inspector で指定できる
 
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
-        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.rigid2D = GetComponent<Rigidbody2D>();  // 毎回参照する必要がないため、 Start 時に一度取得して、Update 時に必要に応じて使用する
     }
 
     // Update is called once per frame
